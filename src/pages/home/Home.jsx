cdc2c3daee58af246b0590d2da155b3b6bf4fb08
jaime chico loayza <jrchldev@gmail.com>
2024-09-03 12:47:52 -0500

    styles tailwind add

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,9 +3,9 @@ import React from "react";
 
 const Home = () => {
   return (
-
       <>
         <div>
+          <h1>Hola mundo</h1>
         </div>
       </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,27 @@ export default {
   ],
   theme: {
     extend: {
+      colors: {
+        //textos
+        "primary-100": "#F8F8FF",
+        "secondary-100":"#9EA2A8",
+        "tertiary-100": "#58DDA3",
+        
+        //fondos
+        "bg-100": "#1B1B1B",
+        
+      },
+      backgroundImage: {
+        "footer": "url('/src/assets/bFooter.png')",
+      },
+      fontSize: {
+        "dynamic-p": "clamp(0.875rem, 0.875rem + 0vw, 0.875rem);",
+        "dynamic-a": "clamp(1rem, 0.9286rem + 0.3571vw, 1.25rem);",
+        "dynamic-li": "clamp(1rem, 1rem + 0vw, 1rem)",
+        "dynamic-h2": "clamp(1.5rem, 1.2143rem + 1.4286vw, 2.5rem);",
+        "dynamic-h4": "clamp(1.125rem, 0.9107rem + 1.0714vw, 1.875rem);",
+        "dynamic-h6": "clamp(1rem, 0.9286rem + 0.3571vw, 1.25rem);",
+      },
     },
   },
   plugins: [],
