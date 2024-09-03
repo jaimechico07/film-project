c55d8f44d6eb58cca5346db0afa59302ca3a26ca
jaime chico loayza <jrchldev@gmail.com>
2024-09-02 21:26:17 -0500

    project Structure

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,9 +2,11 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Hola mundo</h1>
-    </div>
+
+      <>
+        <header></header>
+        <section-hero></section-hero>
+      </>
   );
 };
 
