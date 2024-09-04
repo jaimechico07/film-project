79f1f835776c12806d13925efda9d25a80dc9426
jaime chico loayza <jrchldev@gmail.com>
2024-09-03 23:19:51 -0500

    mejorando estructura

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,11 +3,9 @@ import React from "react";
 
 const Home = () => {
   return (
-      <>
-        <div>
+        <main>
           <h1>Hola mundo</h1>
-        </div>
-      </>
+        </main>
   );
 };
 
